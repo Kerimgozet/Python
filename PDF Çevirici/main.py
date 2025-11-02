35bd51c4caf5a26c25bae90b39bde0ff8f3c4cc1
Kerim Gözet <kerimgozet4@Gmail.com>
2025-11-02 22:24:24 +0300

    Delete PDF Çevirici directory

--- PDF Çevirici/main.py
+++
@@ -1,8 +0,0 @@
-from pdf2docx import Converter
-
-pdf_file="unique-tower-139460.pdf"
-docx_file="unique-tower-139460.docx"
-
-cv=Converter(pdf_file=pdf_file)
-cv.convert(docx_file=docx_file)
-cv.close()
